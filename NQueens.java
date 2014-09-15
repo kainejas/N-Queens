422e49f7a39b6b86649e7aba05699d921a9c0349
kainejas <kainejas@uga.edu>
2014-09-15 08:47:50 -0400

    Fixed selection and mutation
    Algorithm finds answers much faster than before thanks to a new
    crossover operator.

--- NQueens.java
+++ NQueens.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
@@ -8,11 +9,11 @@ public class NQueens {
 	ArrayList<Integer[]> population;
 	ArrayList<Integer> fitnesses;
 	int n;
-	
+	Random rand = new Random();
 	
 	
 	public static void main(String[] args) {
-		NQueens queens = new NQueens(14);
+		NQueens tsp = new NQueens(200);
 		
 	}
 	
@@ -21,16 +22,35 @@ public class NQueens {
 		population = new ArrayList<Integer[]>();
 		fitnesses = new ArrayList<Integer>();
 		
-		Random rand = new Random();
-		for(int i = 0; i < 2000; i++) {
+		
+		for(int i = 0; i < 100; i++) {
 			population.add(new Integer[n]);
 			for(int j = 0; j < n; j ++) {
-				population.get(i)[j] = rand.nextInt(n);
+				population.get(i)[j] = j;
+			}
+			int temp = rand.nextInt(20);
+			for(int k = 0; k < temp; k++) {
+				mutate(i);
 			}
 		}
 		
-		
+		 
 		simulate();
+		
+	}
+	
+	public void mutate(int index) {
+		
+		int index1 = rand.nextInt(n);
+		int index2 = rand.nextInt(n);
+		
+		int val1 = population.get(index)[index1];
+		int val2 = population.get(index)[index2];
+		
+		population.get(index)[index2] = val1;
+		population.get(index)[index1] = val2;
+		
+		
 		
 	}
 	
@@ -38,9 +58,15 @@ public class NQueens {
 		int count = 0;
 		
 
-	while(!solutionFound() && count < 1000000){
+	while(!solutionFound() && count < population.size()*1000000){
 
-		System.out.println(count + "\tAvg:" + averageFitness());
+	//	System.out.println(count + "\tAvg:" + averageFitness());
+		//System.out.println(count);
+	
+		if(count % 1000 == 0) {
+			System.out.println("Crossovers: " + (count) + "\tAvg Fitness:" + averageFitness());
+		}
+	
 		crossOver();
 		count++;
 	}
@@ -85,12 +111,11 @@ public int[] findParents() {
 		List<Integer[]> parentsList = new ArrayList<Integer[]>();
 		
 	
-		Random rand = new Random();
 		
 		int bestFit = Integer.MAX_VALUE;
 		int bestIndex = -1;
 		
-		for(int i = 0; i < 5; i ++) {
+		for(int i = 0; i < 10; i ++) {
 			int index = rand.nextInt(population.size());
 			Integer[] tempArr = {fitness(population.get(index)) , index};
 			parentsList.add(tempArr);
@@ -143,27 +168,58 @@ public int[] findParents() {
 		Integer[] parent1 = population.get(indexes[0]);
 		Integer[] parent2 = population.get(indexes[1]);
 		
-		Random rand = new Random();
-		int crossOverPoint = rand.nextInt(n);
+		int crossOverPoint = 0;//rand.nextInt(n);
 		
-		Integer[] child1 = new Integer[n];
-		Integer[] child2 = new Integer[n];
+		int probability = rand.nextInt(3);
+		if(probability == 0) {
+			crossOverPoint = n/2 -1;
+		}
+		else if(probability == 1) {
+			crossOverPoint = n/2;
+		}
+		else if(probability == 2) {
+			crossOverPoint = n/2+1;
+		}
+		
+		
+		
+	
+		
+		ArrayList<Integer> child1 = new ArrayList<Integer>(n);
+		ArrayList<Integer> child2 = new ArrayList<Integer>(n);
 		
 		//0 to crossoverpoint from first
 		// crossoverpoint to n
 		
 		for(int i = 0; i < crossOverPoint; i++) {
-			child1[i] = parent1[i];
-			child2[i] = parent2[i];
+			child1.add(parent1[i]);
+			child2.add( parent2[i]);
 		}
+		
+		
 		
 		for(int j = crossOverPoint; j < n; j++) {
-			child1[j] = parent2[j];
-			child2[j] = parent1[j];
+			if(!child1.contains(parent2[j])){
+			child1.add(parent2[j]);
+			}
+			if(!child2.contains(parent1[j])){
+			child2.add(parent1[j]);
+			}
 		}
 		
-		population.add(child1);
-		population.add(child2);
+		for(int k = 0; k < crossOverPoint; k++) {
+			if(!child1.contains(parent2[k]))
+				child1.add(parent2[k]);
+			
+			if(!child2.contains(parent1[k]))
+				child2.add(parent1[k]);
+		}
+		
+		
+		Integer[] tempArr1 = Arrays.copyOf(child1.toArray(), child1.toArray().length, Integer[].class);
+		Integer[] tempArr2 = Arrays.copyOf(child2.toArray(), child1.toArray().length, Integer[].class);
+		population.add(tempArr1);
+		population.add(tempArr2);
 		
 		findWorstTwo();
 		
@@ -175,6 +231,12 @@ public int[] findParents() {
 		int nextIndex = -1;
 		int nextFitness = -1;
 		for(int i = 0; i < population.size(); i++) {
+			
+			double probability = rand.nextDouble();
+			if(probability <= 0.03) {
+				mutate(i);
+			}
+			
 			int fit = fitness(population.get(i));
 			if(fit > maxFitness) {
 				maxFitness = fit;
